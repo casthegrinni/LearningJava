0e9546f5664e719aa593746cf3549e0715ef7d71
Aluno Bandtec <github@alunos.bandtec.com.br>
2021-08-05 21:27:54 -0300

    indo embora

---
+++ first-java-lesson/src/main/java/br/com/bandtec/first/java/lesson/FirstProgram.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.bandtec.first.java.lesson;
+
+/**
+ *
+ * @author Aluno
+ */
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.print("Hello World The curse is broken now...");
+        System.out.println(" Are u fine???");
+        
+    }
+}
