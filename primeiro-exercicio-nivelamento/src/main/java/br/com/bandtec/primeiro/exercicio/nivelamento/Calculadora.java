1739b9c2f43dd65b0f7b9b9fbafb0b1d6c22d2b8
Aluno Bandtec <github@alunos.bandtec.com.br>
2021-08-12 21:18:23 -0300

    teste

---
+++ primeiro-exercicio-nivelamento/src/main/java/br/com/bandtec/primeiro/exercicio/nivelamento/Calculadora.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.bandtec.primeiro.exercicio.nivelamento;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Calculadora {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        Double primeiroNumero;
+        Double segundoNumero;
+        
+        System.out.println("Calculadora \nDigite um número real:" );
+        primeiroNumero = leitor.nextDouble();
+        
+        System.out.println("Digite um segundo número");
+        segundoNumero = leitor.nextDouble();
+        System.out.println("Primeiro número: " + primeiroNumero + ", Segundo número: " + segundoNumero);
+        System.out.println("Soma: " + (primeiroNumero + segundoNumero));
+        System.out.println("Subtração: " + (primeiroNumero - segundoNumero));
+        System.out.println("Multiplicação: " + (primeiroNumero * segundoNumero));
+        System.out.println("Divisão: " + (primeiroNumero / segundoNumero));
+    }
+}
